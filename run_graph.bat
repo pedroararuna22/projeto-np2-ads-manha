da034ee091023f372d3960c2108d0c33d7fb8ac0
pedroararuna22 <pedroararuna7@gmail.com>
2025-11-20 20:43:41 -0300

    Alterações iniciais na branch dev

--- run_graph.bat
+++ run_graph.bat
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 @echo off
 python grafo\app_graph.py
 pause
+=======
+@echo off
+python grafo\app_graph.py
+pause
+>>>>>>> fabbfa9 (salvando alterações locais antes do pull)
--- run_tree.bat
+++ run_tree.bat
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 @echo off
 python arvore\app_tree.py
 pause
+=======
+@echo off
+python arvore\app_tree.py
+pause
+>>>>>>> fabbfa9 (salvando alterações locais antes do pull)
